264c2da1754455d4856b628c37574904be66a566
Sakhura <sabina.romero@gmail.com>
2026-03-26 19:54:57 -0300

    update

---
+++ config/database.js
@@ -0,0 +1,20 @@
+const { pool } = require('pg')
+require('dotenv').config();
+
+const pool = new Pool ({
+    host:     process.env.DB_HOST,
+    port:     process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user:     process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
+
+pool.connect((err, Client, release) =>{
+    if (err){
+        return console.error('Error al conectar: ', err.message);
+    }
+    console.log('Conexion Exitosa');
+    release();
+});
+
+module.exports = pool;
